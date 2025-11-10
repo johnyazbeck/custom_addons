5bef452c5905c5484c5bd94e2c77401328eec4a4
johnyazbeck <125282806+johnyazbeck@users.noreply.github.com>
2025-11-11 00:12:36 +0100

    try
    try

--- my_bike_shop/__manifest__.py
+++ my_bike_shop/__manifest__.py
@@ -10,7 +10,7 @@
         - Reporting des ventes et locations
     """,
     'author': "Yazbeck John et Jose Bigoro",
-    'website': "https://github.com/johnyazbeck/custom_addons",
+    'website': "https://github.com/johnyazbeck/",
     'category': 'Sales',
     'version': '0.1',
     'depends': ['base', 'sale', 'stock', 'product'],  # Rétablir toutes les dépendances
